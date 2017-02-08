482cf144e8df0c097202a3b2c87806d47f38868a
Thomas Pelletier <pelletier.thomas@gmail.com>
2017-02-07 18:35:04 -0800

    Release to clojar

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/pelletier/lein-dynamic-version"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]])
