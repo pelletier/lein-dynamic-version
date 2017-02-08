13e2a7a7e99cb7a39669f739f8ca8f9400a972e1
Thomas Pelletier <pelletier.thomas@gmail.com>
2017-02-07 19:22:03 -0800

    Add travis

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]])
+  :repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]]
+  :profiles {:clojure-1.8.0 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :clojure-dev {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}})
